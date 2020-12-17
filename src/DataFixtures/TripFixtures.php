5db03aeb6e9209d539930608cc011ee885556def
Enzo <enzogaudry1@gmail.com>
2020-12-17 07:28:28 +0100

    fix date range

--- src/DataFixtures/TripFixtures.php
+++ src/DataFixtures/TripFixtures.php
@@ -13,10 +13,13 @@ class TripFixtures extends Fixture
     public function load(ObjectManager $manager)
     {
         $generator = Faker\Factory::create("en_US");
+        $startDate = "-1 year";
+        $endDate = "1 year";
         for ($i = 0; $i < 10; $i++) {
             $trip = new Trip();
             $trip
-                ->setDate($generator->date)
+                ->setDate($generator->unique()
+                                    ->dateTimeBetween($startDate, $endDate)->format('Y-m-d'))
                 ->setAddressStart($generator->city)
                 ->setAddressEnd($generator->city)
                 ->setNbPassengers($generator->numberBetween(1, 4));
