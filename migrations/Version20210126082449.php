aa1bb4984b0f5422e97c6068219804fdb0ebdee8
EnzoGaudry <enzogaudry1@gmail.com>
2021-01-26 11:18:26 +0100

    feat new migration and fix style

---
+++ migrations/Version20210126082449.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20210126082449 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE user ADD phone INT DEFAULT NULL, ADD bio LONGTEXT DEFAULT NULL, ADD vehicle VARCHAR(255) DEFAULT NULL');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE user DROP phone, DROP bio, DROP vehicle');
+    }
+}
